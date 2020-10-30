1cbb455058c3c0cf7a85c2a73f2b7be2c3a55ff7
Czerwcuu <czerwiecr1999@gmail.com>
2020-10-30 12:50:54 +0100

    builders logger

--- src/main/java/stylowy/creativecontrol/CheckCreativeEvent.java
+++ src/main/java/stylowy/creativecontrol/CheckCreativeEvent.java
@@ -1,6 +1,7 @@
 package stylowy.creativecontrol;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
@@ -19,8 +20,17 @@ public class CheckCreativeEvent implements Listener {
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         if(item != Material.AIR) {
             LogToFile log = new LogToFile();
-            String sum =  "[" + format.format(now) + "]" + name + " took " + itemname + " x "+ilosc+" from CREATIVE mode";
-            log.logToFile(sum);
+            LogToFile_Builders log_build = new LogToFile_Builders();
+            Player p = (Player)e.getWhoClicked();
+            if(p.getName().equals("puqqe") || p.getName().equals("Kmuhi") || p.getName().equals("bvdzik")){
+                String sum =  "[" + format.format(now) + "]" + name + " took " + itemname + " x "+ilosc+" from CREATIVE mode";
+                log_build.logToFile(sum);
+            }
+            else{
+                String sum =  "[" + format.format(now) + "]" + name + " took " + itemname + " x "+ilosc+" from CREATIVE mode";
+                log.logToFile(sum);
+            }
+
         }
     }
 }
